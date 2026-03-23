1cdc2480a8151bac76fc1e6201348aa96273af42
kareldigital1 <anselmeseho6@gmail.com>
2026-03-23 13:30:19 +0100

    mon propjet

---
+++ Agence/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render , redirect
+from .form import CustomUserCreationForm
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
+
+# Create your views here.
+def home(request):      
+    return render(request, 'home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login_view')
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'register.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.error(request, 'Invalid username or password')
+    return render(request, 'login_view.html') 
